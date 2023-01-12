febf164d4f3396f71fc17f8ad286973bdb2a2c9d
Letícia <leticiaalaura@gmail.com>
2023-01-12 19:51:14 -0300

    cadastro de pets

--- adote/urls.py
+++ adote/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from adote import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('usuarios.urls'))
-]
+    path('auth/', include('usuarios.urls')),
+    path('divulgar/', include('divulgar.urls')),
+    path('adotar/', include('adotar.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
